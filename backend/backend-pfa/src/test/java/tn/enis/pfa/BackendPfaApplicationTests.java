8fc0d234cfbe6a46be9127b1396f511466d86ee1
aminfr03 <amin.frikha03@gmail.com>
2026-01-30 20:00:19 +0100

    hethi el commit elli fiha el project backend spring

---
+++ backend/backend-pfa/src/test/java/tn/enis/pfa/BackendPfaApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.enis.pfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
