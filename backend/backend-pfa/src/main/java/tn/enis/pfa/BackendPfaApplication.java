f61461e23b9aeb67df6672ae504127a9049368f6
aminfr03 <amin.frikha03@gmail.com>
2026-01-30 20:10:14 +0100

    baddelt haja

--- backend/backend-pfa/src/main/java/tn/enis/pfa/BackendPfaApplication.java
+++ backend/backend-pfa/src/main/java/tn/enis/pfa/BackendPfaApplication.java
@@ -8,6 +8,8 @@ public class BackendPfaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendPfaApplication.class, args);
+		
+		System.out.println("hello amin");
 	}
 
 }
