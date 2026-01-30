fa255e57e130617346b870a86bef4c56c2cd2887
aminfr03 <amin.frikha03@gmail.com>
2026-01-30 20:00:19 +0100

    hethi el commit elli fiha el project backend spring

---
+++ backend/backend-pfa/src/main/java/tn/enis/pfa/BackendPfaApplication.java
@@ -0,0 +1,13 @@
+package tn.enis.pfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPfaApplication.class, args);
+	}
+
+}
