7d6aa58e3f0d79b6b226847a0237dbee81aa0aa1
Wassim Abdelhedi <wassim.abdelhedi@enis.tn>
2026-02-11 18:18:01 +0100

    Nettoyage complet de main et création de dossiers vides backend, frontend, database

--- backend/backend-pfa/src/main/java/tn/enis/pfa/repository/ChapterRepository.java
+++
@@ -1,8 +0,0 @@
-package tn.enis.pfa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.enis.pfa.entity.Chapter;
-
-public interface ChapterRepository extends JpaRepository<Chapter, Long> {
-
-}
--- backend/backendPfa/src/main/java/tn/enis/pfa/security/CurrentUserId.java
+++
@@ -1,9 +0,0 @@
-package tn.enis.pfa.security;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.PARAMETER})
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface CurrentUserId {
-}
